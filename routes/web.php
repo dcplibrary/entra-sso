aaccf3fe2af12986cf0d321188bba7dfe8b9218b
Brian Lashbrook <blashbrook@dcplibrary.org>
2025-11-01 20:28:07 -0500

    fix: modify login route

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::middleware(['web'])->group(function () {
     Route::get('/auth/entra', [EntraSSOController::class, 'redirect'])->name('entra.login');
     Route::get('/auth/entra/callback', [EntraSSOController::class, 'callback'])->name('entra.callback');
     Route::post('/auth/entra/logout', [EntraSSOController::class, 'logout'])->name('entra.logout');
-});
-// SSO Routes (provided by package automatically)
-// Login page
-Route::middleware(['web'])->group(function () {
     Route::get('/login', function () {
         return view('entra-sso::auth.login');
     })->name('login');
